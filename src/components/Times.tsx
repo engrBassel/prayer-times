65a12d7b77efd2abcdc94e4cbfb4b16386077979
engrBassel <engrbassel@outlook.com>
2024-05-02 17:44:15 +0300

    Solving images bug

--- src/components/Times.tsx
+++ src/components/Times.tsx
@@ -1,9 +1,16 @@
+import fajrImage from "/src/assets/images/Fajr.jpg";
+import dhuhrImage from "/src/assets/images/Dhuhr.jpg";
+import asrImage from "/src/assets/images/Asr.jpg";
+import maghribImage from "/src/assets/images/Maghrib.jpg";
+import ishaImage from "/src/assets/images/Isha.jpg";
 import Grid from "@mui/material/Unstable_Grid2";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { TimesType, TimeNameType } from "../App";
+
+const images = [fajrImage, dhuhrImage, asrImage, maghribImage, ishaImage];
 
 function Times({
   times,
@@ -24,7 +31,7 @@ function Times({
           <Card>
             <CardMedia
               sx={{ height: 150, width: 200 }}
-              image={`/src/assets/images/${timesNames[indx].name}.jpg`}
+              image={images[indx]}
               title={`${timesNames[indx].name} Prayer`}
             />
             <CardContent>
